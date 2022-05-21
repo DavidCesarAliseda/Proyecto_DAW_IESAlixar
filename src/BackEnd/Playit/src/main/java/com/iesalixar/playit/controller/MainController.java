0ecfac4eb2bfc5fcb53ddde9780b730fe3138434
DavidCesarAliseda <72818962+DavidCesarAliseda@users.noreply.github.com>
2022-05-21 13:42:23 +0200

    Fondo index, login y main_admin

--- src/BackEnd/Playit/src/main/java/com/iesalixar/playit/controller/MainController.java
+++ src/BackEnd/Playit/src/main/java/com/iesalixar/playit/controller/MainController.java
@@ -1,10 +1,13 @@
 package com.iesalixar.playit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.iesalixar.playit.model.JPAUserDetails;
 import com.iesalixar.playit.service.UsuarioServiceImpl;
 
 @Controller
@@ -14,7 +17,12 @@ public class MainController {
 	UsuarioServiceImpl usuarioService;
 	
 	@RequestMapping("/main")
-	public String main() {
+	public String main(Model model) {
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		JPAUserDetails user = (JPAUserDetails) auth.getPrincipal();
+		System.out.println(user.getUsername());
+		model.addAttribute("user", user.getUsername());
 		return "main";
 	}
 }
